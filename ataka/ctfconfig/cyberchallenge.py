96a5997d7631b2de6377959e29ae85055fff1083
iTzVirtual00 <58611844+iTzVirtual00@users.noreply.github.com>
2025-06-18 00:23:46 +0200

    update ctfconfig for cyberchallenge

--- ataka/ctfconfig/cyberchallenge.py
+++ ataka/ctfconfig/cyberchallenge.py
@@ -7,16 +7,18 @@ from collections import defaultdict
 ### EXPORTED CONFIG
 
 CC_TEAM_ID = "6"
+CC_NOP_TEAM_ID = "0"
+CC_GAME_SERVER_IP = '10.10.0.1'
+CC_TEAM_TOKEN = 'AAAAAAAAAAAAAAAAAAAAAAAAAAAAA'
 
 # Ataka Host Domain / IP
-ATAKA_HOST = 'ataka.h4xx.eu'
-GAME_SERVER_IP = '10.10.0.1'
+ATAKA_HOST = '10.91.142.1:8000'
 
 # Default targets for atk runlocal
-RUNLOCAL_TARGETS = ["10.60.0.1"]
+RUNLOCAL_TARGETS = [f"10.60.{CC_NOP_TEAM_ID}.1"]
 
 # IPs that are always excluded from attacks.
-STATIC_EXCLUSIONS = {f'10.60.0.{CC_TEAM_ID}', '10.60.0.1'}
+STATIC_EXCLUSIONS = {f'10.60.{CC_TEAM_ID}.1', f'10.60.{CC_NOP_TEAM_ID}.1'}
 
 ROUND_TIME = 120
 
@@ -28,14 +30,12 @@ FLAG_BATCHSIZE = 69
 FLAG_RATELIMIT = 1  # Wait in seconds between each call of submit_flags()
 
 # When the CTF starts
-START_TIME = datetime.fromisoformat("2025-06-17T12:34:56+02:00")
+START_TIME = int(datetime.datetime.fromisoformat("2024-06-15T10:11:11"))
 
 
 ### END EXPORTED CONFIG
 
-
 valid_flags = set()
-submitted_flags = set()
 services: list[str] = []
 
 def parse_flag(flag: str) -> tuple[int, int, int]:
@@ -44,11 +44,10 @@ def parse_flag(flag: str) -> tuple[int, int, int]:
     service_number = int(flag[4:6], 36)
     return (round_number, team_number, service_number)
 
-
 def get_targets():
     global services
 
-    r = requests.get(f"http://{GAME_SERVER_IP}:8081/flagIds", timeout=10)
+    r = requests.get(f"http://{CC_GAME_SERVER_IP}:8081/flagIds", timeout=10)
     data = r.json()
     if not services:
         services = list(data.keys())
@@ -68,19 +67,51 @@ def get_targets():
     return dict(targets)
 
 
-def _randomness():
-    import random
-    return \
-        random.choices([FlagStatus.OK, FlagStatus.INVALID, FlagStatus.INACTIVE, FlagStatus.OWNFLAG, FlagStatus.ERROR],
-                       weights=[0.5, 0.2, 0.2, 0.05, 0.1], k=1)[0]
+def parse_submission(sub: str) -> FlagStatus:
+    status = sub["status"]
+    msg = sub["msg"].lower()
 
+    if status == "ACCEPTED":
+        return FlagStatus.OK
+
+    if status == "RESUBMIT":
+        # not active yet
+        return FlagStatus.QUEUED
+
+    if status == "ERROR":
+        return FlagStatus.ERROR
+
+    # status == "DENIED"
+    if "invalid flag" in msg:
+        return FlagStatus.INVALID
+    if "nop team" in msg:
+        return FlagStatus.NOP
+    if "your own" in msg:
+        return FlagStatus.OWNFLAG
+    if "too old" in msg:
+        return FlagStatus.INACTIVE
+    if "already claimed" in msg:
+        # we already submitted and got told so
+        return FlagStatus.DUPLICATE
+    if "didn't terminate successfully" in msg:
+        # treat dispatch‐failure as an error
+        return FlagStatus.ERROR
+
+    # fallback
+    return FlagStatus.UNKNOWN
 
 def submit_flags(flags):
-    import time
-    global submitted_flags
-    global valid_flags
-    time.sleep(min(len(flags) / 1000, 2))
-    result = {flag: FlagStatus.DUPLICATE if flag in submitted_flags else _randomness() for flag in flags}
-    submitted_flags.update([flag for flag, status in result.items() if status != FlagStatus.ERROR])
-    valid_flags.update(set(map(parse_flag, [flag for flag, status in result.items() if status == FlagStatus.OK])))
-    return [result[flag] for flag in flags]
+    flags = {flag: i for i, flag in enumerate(flags)}
+
+    data = requests.put(f'http://{CC_GAME_SERVER_IP}:8080/flags', headers={
+        'X-Team-Token': CC_TEAM_TOKEN
+    }, json=flags).json()
+
+    result = [FlagStatus.ERROR]*len(flags)
+    for flag_response in data:
+        flag = flag_response['flag']
+        status = parse_submission(flag_response)
+        result[flags[flag]] = status
+        if status == FlagStatus.OK:
+            valid_flags.add(parse_flag(flag))
+    return result
